0c331d2032991c4404f0f975fa5d5c8f41d6a9c6
Gustavo Giron <gustavo.giron@ges.inatel.br>
2026-08-11 14:07:15 -0300

    Add files via upload

---
+++ Aula 1/src/main.java
@@ -0,0 +1,10 @@
+package src;
+public class main { 
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int x = 10;
+        int y = 5;
+        int z = 3;
+        System.out.println(x+y);
+    }
+}
